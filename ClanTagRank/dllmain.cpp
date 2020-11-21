60acdf3f8b441525f4a26e049fb0f3a7ebb51ab5
Federico Cecchetto <fedecek3@gmail.com>
2020-11-21 21:25:43 +0100

    Add rename command

--- ClanTagRank/dllmain.cpp
+++ ClanTagRank/dllmain.cpp
@@ -11,21 +11,24 @@ utils::memory::allocator allocator;
 
 std::string clantags[18];
 
+std::string names[18];
+
 void set_clan_tag(int clientNum, std::string clantag)
 {
     clantags[clientNum] = clantag;
     game::ClientUserInfoChanged(clientNum);
+
     printf("setting clantag to %s for client %i\n", clantag.data(), clientNum);
 }
 
 const char* clantag_stub(const char* s, const char* key, const char* result, int clientNum)
 {
-    if (clantags[clientNum].empty())
-    {
-        return result;
-    }
+    return clantags[clientNum].empty() ? result : clantags[clientNum].data();
+}
 
-    return clantags[clientNum].data();
+const char* name_stub(const char* s, const char* key, const char* result, int clientNum)
+{
+    return names[clientNum].empty() ? result : names[clientNum].data();
 }
 
 const char* using_tag_stub(const char* s, const char* key, const char* result, int clientNum)
@@ -38,6 +41,8 @@ void init()
     userinfo::add("ec_TagText", clantag_stub);
     userinfo::add("clanAbbrev", clantag_stub);
     userinfo::add("clanAbbrevEv", clantag_stub);
+
+    userinfo::add("name", name_stub);
 
     userinfo::add("ec_usingTag", using_tag_stub);
 
@@ -58,7 +63,7 @@ void init()
         strncpy_s(clean_tag, clantag, 7);
         game::I_CleanStr(clean_tag);
 
-        set_clan_tag(clientNum, std::string(clean_tag));
+        set_clan_tag(clientNum, clean_tag);
     });
 
     command::add("setclantags", [](command::params& params)
@@ -73,7 +78,7 @@ void init()
 
         for (auto i = 0; i < sv_maxclients; i++)
         {
-            set_clan_tag(i, std::string(clean_tag));
+            set_clan_tag(i, clean_tag);
         }
     });
 
@@ -90,7 +95,7 @@ void init()
             return;
         }
 
-        set_clan_tag(clientNum, std::string(clantag));
+        set_clan_tag(clientNum, clantag);
     });
 
     command::add("setclantagsraw", [](command::params& params)
@@ -101,8 +106,27 @@ void init()
 
         for (auto i = 0; i < sv_maxclients; i++)
         {
-            set_clan_tag(i, std::string(clantag));
+            set_clan_tag(i, clantag);
         }
+    });
+
+    command::add("rename", [](command::params& params)
+    {
+        const auto client = params.get(1);
+        const auto name = params.get(2);
+
+        const auto clientNum = atoi(client);
+        const auto sv_maxclients = game::get_maxclients();
+
+        if (clientNum >= sv_maxclients)
+        {
+            return;
+        }
+
+        names[clientNum] = name;
+        game::ClientUserInfoChanged(clientNum);
+
+        printf("renamed client %i to %s\n", clientNum, name);
     });
 }
 
