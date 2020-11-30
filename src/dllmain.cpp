55ef761b3c3b684c33bef1f8f6592448528e14c9
Federico Cecchetto <fedecek3@gmail.com>
2020-11-30 10:09:00 +0100

    Remove this

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -28,11 +28,6 @@ const char* using_tag_stub(const char* s, const char* key, const char* result, i
 
 void init()
 {
-    chat::add("!test", [](int clientNum, command::params_sv& params, std::function<void()> next)
-    {
-        chat::tellraw(clientNum, "test");
-    });
-
     userinfo::add("ec_TagText", clantag_stub);
     userinfo::add("clanAbbrev", clantag_stub);
     userinfo::add("clanAbbrevEv", clantag_stub);
